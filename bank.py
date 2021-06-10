fd5a36f06de79cbaf3415aef3995b30b31f3db67
Kaylindaww <akullukaylinda@gmail.com>
2021-06-10 11:56:09 +0300

    first commit

--- bank.py
+++ bank.py
@@ -1,33 +1,88 @@
+from datetime import datetime as dt
+import datetime
+
 class Account:
     def __init__(self,name,phone):
-        self.name=name
-        self.phone=phone
-        self.balance=0
-        self.transactionfee=200
-        self.loan=0
-        self.loan_fees=0.05
-        self.loan_limit=3000
+        self.name = name
+        self.phone = phone
+        self.balance = 0
+        self.transaction_fee = 30
+        self.loan = 0
+        self.loan_limit = 5000
+        self.loan_interest = 5 
+        self.transactions = []
+
     def deposit(self,amount):
         if amount<=0:
-            return "Please input a valid amount"
+            return f"please deposit a valid amount"
+
         else:
-            self.balance+=amount
-            return f"Hello {self.name} you have deposited {amount} your new balance is {self.balance}"
-    def withdraw(self,amount):
-        total=self.transactionfee+amount
-        if amount<=0:
-            return "Cannot withdraw zero or less "
-        elif total<amount:
-            return "Cannot withdraw you have a less money"
+            self.balance += amount
+            transaction = {"amount":amount,"balance":self.balance,"narration":"You deposited","time":dt.now()}
+            self.transactions.append(transaction)
+            return f"Hello, {self.name} you have deposited {amount} ,new balance is {self.balance}"
+    
+
+     
+    def withdraw(self,withdrawal_amount):
+        total = withdrawal_amount + self.transaction_fee
+        if withdrawal_amount <= 0:
+            return "please enter valid amount"
+
+        elif total > self.balance:
+            return "Insufficient balance"  
+
         else:
-            self.balance-=total
-            return f"Hello {self.name} you have withdrawn {amount} and your balance is {self.balance-(self.transactionfee+amount)} and your transaction fee is {self.transactionfee}"
+
+            self.balance -= total
+            transaction = {"amount":withdrawal_amount,"balance":self.balance,"narration":"You withdrew", "time":dt.now()}
+            self.transactions.append(transaction)
+            return f"Hello {self.name} you have successfully withdrawn {withdrawal_amount} account balance is {self.balance}"
+
     def borrow(self,loan):
-        5/100*100
-        total=self.loan
-        if loan<=0:
-            return"you can  not borrow a loan"
-        elif loan>=0:
-            return "you have an outstanding balance"
+        loan_fees = self.loan_interest * loan / 100
+        
+        if loan < 0:
+            return "Please enter a valid amount"
+
+        elif loan > self.loan_limit:
+            return "The amount exeedes the loan limit,please enter a lower amount"
+
+        elif  self.loan > 0:
+            return "You have an outstanding loan"
+
         else:
-            return f"hello {self.name} you can borrow {amount} from us"        
+            repay = loan + loan_fees
+            self.loan += repay
+            self.balance += loan
+            transaction = {"amount":loan,"balance":self.balance,"narration":"You borrowed","time":dt.now()}
+            self.transactions.append(transaction)
+            return f"You have received a loan of {loan} your account balance now is {self.balance},the amount you will repay is {self.loan}"
+
+    def repay(self,repay_amount):
+        if repay_amount < 0:
+            return "Please enter a valid amount"
+
+        elif repay_amount < self.loan:
+            self.loan = self.loan - repay_amount
+            transaction = {"amount":repay_amount,"balance":self.balance,"narration":"You repaid loan","time":dt.now()}
+            self.transactions.append(transaction)
+            return f"You have repaid {repay_amount} outstanding loan is {self.loan}"
+
+        elif repay_amount > self.loan:
+            excess = repay_amount - self.loan
+            self.balance += excess
+            transaction = {"amount":repay_amount,"balance":self.balance,"narration":"You repaid loan","time":dt.now()}
+            self.transactions.append(transaction)
+            return f"You have fully settled your loan, account balance is {self.balance}"
+
+    def get_statement(self):
+        for transaction in self.transactions:
+            amount = transaction["amount"]
+            narration = transaction["narration"]
+            balance = transaction["balance"]
+            time = transaction["time"]
+            date = time.strftime("%D")
+            print(f"{date}...{narration} {amount}.....Balance {balance}")
+
+            
