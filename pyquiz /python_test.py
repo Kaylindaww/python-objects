6e9dbb14585846e4b7af87d3f58499b836586d70
Kaylindaww <akullukaylinda@gmail.com>
2021-06-17 10:45:09 +0300

    first commit

---
+++ pyquiz /python_test.py
@@ -0,0 +1,43 @@
+#6.
+def divisible_by_seven():
+    x= range(100,200)
+    for y  in x:
+        if x%7==0:
+             print(y)
+
+
+#1
+x = [100,110,120,130,140,150]  
+newlist=[] 
+for newlist in x:
+    if x*5==2:
+        print(newlist)
+        
+#4
+def smallest():
+    list=([3,6,8,2,4,1,5,7])
+    list.sort()
+    print(list.sort[:1])
+#7
+def student(age,name):
+    student=[{"age": 19, "name": "Eunice"},{"age": 21, "name": "Agnes"},  {"age": 18, "name": "Teresa"},{"age": 22, "name": "Asha"}] 
+    print("hello" + name + age)
+    
+
+#2.
+def divisible_by_three():
+    n=100
+    x=range(1,n)
+    for y in x:
+        if y%3==0:
+            print(y) 
+
+#5.
+def removes_duplicate():
+    x = ['a','b','a','e','d','b','c','e','f','g','h']
+    for y in x:
+        y=set(x)
+    print(x(y))
+#3.
+def flatten():
+    x = [[1,2],[3,4],[5,6]],
